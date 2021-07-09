0f2bf99c2bcc820bdba2eda192f9e87cb8f15160
Juan Felipe Ramirez <juanfeliperamirezsanchez@gmail.com>
2021-07-09 09:13:38 -0500

    Calculadora V1

---
+++ ejercicio.js
@@ -0,0 +1,201 @@
+//variables globales
+let datos = document.getElementById("datos");
+let letra = document.getElementById("letra");
+let numero = document.getElementById("numero");
+let btnA = document.getElementById("btnA");
+let btnB = document.getElementById("btnB");
+let btnC = document.getElementById("btnC");
+let btnD = document.getElementById("btnD");
+let btnE = document.getElementById("btnE");
+let btnF = document.getElementById("btnF");
+let btnG = document.getElementById("btnG");
+let btnH = document.getElementById("btnH");
+let btnI = document.getElementById("btnI");
+let btnJ = document.getElementById("btnJ");
+let btnK = document.getElementById("btnK");
+let btnL = document.getElementById("btnL");
+let btnM = document.getElementById("btnM");
+let btnN = document.getElementById("btnN");
+let btnNE = document.getElementById("btnNE");
+let btnO = document.getElementById("btnO");
+let btnP = document.getElementById("btnP");
+let btnQ = document.getElementById("btnQ");
+let btnR = document.getElementById("btnR");
+let btnS = document.getElementById("btnS");
+let btnT = document.getElementById("btnT");
+let btnU = document.getElementById("btnU");
+let btnV = document.getElementById("btnV");
+let btnW = document.getElementById("btnW");
+let btnX = document.getElementById("btnX");
+let btnY = document.getElementById("btnY");
+let btnZ = document.getElementById("btnZ");
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
+let btn7 = document.getElementById("btn7");
+let btn8 = document.getElementById("btn8");
+let btn9 = document.getElementById("btn9");
+let btn0 = document.getElementById("btn0");
+let btnEspacio = document.getElementById("btnEspacio");
+let btnEnter = document.getElementById("btnEnter");
+
+
+//Eventos
+btnNE.onclick = function() {
+    texto.innerHTML += "ñ";
+};
+
+btnA.onclick = function() {
+    texto.innerHTML += "a";
+};
+
+btnB.onclick = function() {
+    texto.innerHTML += "b";
+};
+
+btnC.onclick = function() {
+    texto.innerHTML += "c";
+};
+
+btnD.onclick = function() {
+    texto.innerHTML += "d";
+};
+
+btnE.onclick = function() {
+    texto.innerHTML += "e";
+};
+
+btnF.onclick = function() {
+    texto.innerHTML += "f";
+};
+
+btnG.onclick = function() {
+    texto.innerHTML += "g";
+};
+
+btnH.onclick = function() {
+    texto.innerHTML += "h";
+};
+
+btnI.onclick = function() {
+    texto.innerHTML += "i";
+};
+
+btnJ.onclick = function() {
+    texto.innerHTML += "j";
+};
+
+btnK.onclick = function() {
+    texto.innerHTML += "k";
+};
+
+btnL.onclick = function() {
+    texto.innerHTML += "l";
+};
+
+btnM.onclick = function() {
+    texto.innerHTML += "m";
+};
+
+btnN.onclick = function() {
+    texto.innerHTML += "n";
+};
+
+btnO.onclick = function() {
+    texto.innerHTML += "o";
+};
+
+btnP.onclick = function() {
+    texto.innerHTML += "p";
+};
+
+btnQ.onclick = function() {
+    texto.innerHTML += "q";
+};
+
+btnR.onclick = function() {
+    texto.innerHTML += "r";
+};
+
+btnS.onclick = function() {
+    texto.innerHTML += "s";
+};
+
+btnT.onclick = function() {
+    texto.innerHTML += "t";
+};
+
+btnU.onclick = function() {
+    texto.innerHTML += "u";
+};
+
+btnV.onclick = function() {
+    texto.innerHTML += "v";
+};
+
+btnW.onclick = function() {
+    texto.innerHTML += "w";
+};
+
+btnX.onclick = function() {
+    texto.innerHTML += "x";
+};
+
+btnY.onclick = function() {
+    texto.innerHTML += "y";
+};
+
+btnZ.onclick = function() {
+    texto.innerHTML += "z";
+};
+
+btn1.onclick = function() {
+    texto.innerHTML += "1";
+};
+
+btn2.onclick = function() {
+    texto.innerHTML += "2";
+};
+
+btn3.onclick = function() {
+    texto.innerHTML += "3";
+};
+
+btn4.onclick = function() {
+    texto.innerHTML += "4";
+};
+
+btn5.onclick = function() {
+    texto.innerHTML += "5";
+};
+
+btn6.onclick = function() {
+    texto.innerHTML += "6";
+};
+
+btn7.onclick = function() {
+    texto.innerHTML += "7";
+};
+
+btn8.onclick = function() {
+    texto.innerHTML += "8";
+};
+
+btn9.onclick = function() {
+    texto.innerHTML += "9";
+};
+
+btn0.onclick = function() {
+    texto.innerHTML += "0";
+};
+
+btnEspacio.onclick = function() {
+    texto.innerHTML += " ";
+};
+
+btnEnter.onclick = function() {
+    texto.innerHTML += "<br>";
+};
